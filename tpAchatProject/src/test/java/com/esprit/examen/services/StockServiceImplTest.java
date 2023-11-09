402530a6e0a508d845f610169745ba730090be05
aminesaadaoui <104393874+aminesaadaoui@users.noreply.github.com>
2023-11-09 12:03:39 +0100

    test Stock

--- tpAchatProject/src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ tpAchatProject/src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,4 +1,4 @@
-/*package com.esprit.examen.services;
+package com.esprit.examen.services;
 
 import static org.junit.Assert.*;
 import java.util.List;
@@ -17,17 +17,16 @@ public class StockServiceImplTest {
 	
 	@Test
 	public void testAddStock() {
-	//	List<Stock> stocks = stockService.retrieveAllStocks();
-	//	int expected=stocks.size();
+		List<Stock> stocks = stockService.retrieveAllStocks();
+		int expected=stocks.size();
 		Stock s = new Stock("stock test",10,100);
 		Stock savedStock= stockService.addStock(s);
 		
-	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
+		assertEquals(expected+1, stockService.retrieveAllStocks().size());
 		assertNotNull(savedStock.getLibelleStock());
 		stockService.deleteStock(savedStock.getIdStock());
 		
-	} 
-	
+	}
 	@Test
 	public void testAddStockOptimized() {
 
@@ -38,8 +37,7 @@ public class StockServiceImplTest {
 		assertTrue(savedStock.getQteMin()>0);
 		stockService.deleteStock(savedStock.getIdStock());
 		
-	} 
-	
+	}
 	@Test
 	public void testDeleteStock() {
 		Stock s = new Stock("stock test",30,60);
@@ -49,4 +47,3 @@ public class StockServiceImplTest {
 	}
 
 }
-*/
