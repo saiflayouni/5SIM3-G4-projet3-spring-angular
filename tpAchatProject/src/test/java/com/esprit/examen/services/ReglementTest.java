a8be98b1577b0c87f4739a2242b27ae50d8023f4
Saif Eddine Layouni <33223873+saiflayouni@users.noreply.github.com>
2023-11-14 19:50:01 +0100

    Update ReglementTest.java

--- tpAchatProject/src/test/java/com/esprit/examen/services/ReglementTest.java
+++ tpAchatProject/src/test/java/com/esprit/examen/services/ReglementTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@SpringBootTest(classes = TpAchatProjectApplication.class)
 @ExtendWith(MockitoExtension.class)
 public class ReglementTest {
     @Autowired
